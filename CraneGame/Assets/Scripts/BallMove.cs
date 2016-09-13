34b01c66a2d423de0fa63f10eb71eb783fcab592
Alicia Stott <as8885@nyu.edu>
2016-09-12 22:42:23 -0400

    metadata I guess

---
+++ CraneGame/Assets/Scripts/BallMove.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallMove : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.position = new Vector3 (transform.position.x - 3 * Time.deltaTime,
+				transform.position.y,
+				transform.position.z);
+		}
+        
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.position = new Vector3 (transform.position.x + 3 * Time.deltaTime,
+				transform.position.y,
+				transform.position.z);
+		}
+        
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			transform.position = new Vector3 (transform.position.x,
+				transform.position.y,
+				transform.position.z - 3 * Time.deltaTime);
+		}
+        
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			transform.position = new Vector3 (transform.position.x,
+				transform.position.y,
+				transform.position.z + 3 * Time.deltaTime);
+		}
+	
+	}
+}
